ecadd417e9b654931cba3c851ba91ad5be123b3d
Michael1117 <1322720377@qq.com>
2019-04-07 02:19:18 +0800

    init

---
+++ 01/01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "<h1>一个标题</h1>";
+    echo "欢迎光临，今天是：";
+    echo date ("Y年m月d日");
+    echo "<br/>";
+    echo date("星期 w")
+?>
+</body>
+</html>
