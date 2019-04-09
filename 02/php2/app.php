a02747042d539c646d6102f11ba4b6101f8bc545
Michael1117 <1322720377@qq.com>
2019-04-09 16:36:51 +0800

    读取目录

--- 02/php2/app.php
+++ 02/php2/app.php
@@ -1,31 +1,18 @@
 <?php
 
-// 更改目录或文件名称  确定原来的目录必须存在
-
-// 注意： 如果原目录和新目录都位于同一心目录下，则认为是该名；
-// 注意： 如果原目录和新目录位于不同父目录下，则认为是移动
-
-/*$olbName = "./itcast";
-
-$newName = "./public";
-
-rename($olbName, $newName);*/
-
-// 目录移动
-/*$olbName = "./public";
-
-$newName = "./itcast/public";
-
-rename($olbName, $newName);*/
-
-
-
-// 打开目录
+// 实例： 读取目录中的条目
 
 $dirname = "./itcast";
 
-// 打开目录， 成功返回目录句柄(资源型),失败返回false
+// 打开目录，返回目录句柄资源
 
 $handle = opendir($dirname);
 
-var_dump($handle);
+// 从目录句柄资源中取回一个条目(文件或目录)
+
+// 循环结束的标志 当资源中所有的条目全部读取完毕 返回false
+while ($line = readdir($handle)) {
+    //echo $line."<br>";
+}
+
+echo $line;
